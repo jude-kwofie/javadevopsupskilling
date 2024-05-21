eec5394b2b468c2cd2757f5ec19aebaffcb37142
jude-kwofie <jude.kwofie@amalitech.com>
2024-05-21 12:27:45 +0000

    feat: added array

--- labs/src/main/java/com/jude/App.java
+++ labs/src/main/java/com/jude/App.java
@@ -10,12 +10,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        StudentArray studentArray = new StudentArray();
+        StudentArray studentArray = new StudentArray(6);
         studentArray.add(new Student("Alex", (short) 24, (byte) 2));
         studentArray.add(new Student("Mex", (short) 24, (byte) 2));
         studentArray.add(new Student("Jude", (short) 24, (byte) 2));
         studentArray.add(new Student("Aaron", (short) 24, (byte) 2));
 
+
+        System.out.println(studentArray.search("Alex"));
 
         System.out.println(studentArray.delete("Jude"));
 //        studentArray.display();
